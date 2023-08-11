80f167f9619d4143964533bd0f03db5aa526a823
jim-low <robil12380@gmail.com>
2023-08-11 22:15:57 +0800

    feat: add simple quick sort algorithm

--- QuickSort.h
+++ QuickSort.h
@@ -1,3 +1,5 @@
+#include <unordered_map>
+
 #pragma once
 
 // quick notes
@@ -7,10 +9,18 @@
 class QuickSort
 {
 public:
-	QuickSort(int* unsorted_array);
-	int* sort();
+	std::unordered_map<int, int> hashMap;
+	int* unsorted;
+	int* sorted;
+	size_t size;
+
+	QuickSort(int* unsorted_array, size_t size);
+	void sort();
 	void display(); // this is where all the fancy fancy stuff gon be shown
 
 private:
 	// this is where all the inner workings will be
+	void quicksort(int* arr, int low, int high);
+	int partition(int* arr, int low, int high); // returns index of pivot element
+	void swap(int* p1, int* p2);
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <ctime>
 #include "omp.h"
 #include "QuickSort.h"
 #include "ParallelQuickSort.h"
 
 int main()
 {
-	omp_set_num_threads(420);
-#pragma omp parallel
+	size_t size = 1000000;
+	int* arr = (int*)calloc(size, sizeof(int));
+	// int size = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < size; ++i)
 	{
-		int ID = omp_get_thread_num();
-		printf("Crush my head with thighs pls from ID %d\n", ID);
+		arr[i] = -800 + (rand() % 1000000);
 	}
+
+	QuickSort quickSort = QuickSort(arr, size);
+
+	clock_t start = clock();
+	quickSort.sort();
+	clock_t end = clock();
+
+	quickSort.display();
+	//std::cout << "it took too much time to print out 1 million numbers so heres a line of text for reference." << std::endl;
+	double elapsedTime = double(end - start) / CLOCKS_PER_SEC;
+
+	std::cout << "Time taken: " << elapsedTime << " seconds" << std::endl;
+
 	return 0;
 }
