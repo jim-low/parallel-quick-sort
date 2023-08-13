b3f6a92d49cc434f9b2f5af41cdb55913fde0857
jim-low <robil12380@gmail.com>
2023-08-13 15:49:53 +0800

    chore: define basic layout for ParallelQuickSort

--- ParallelQuickSort.h
+++ ParallelQuickSort.h
@@ -5,11 +5,18 @@
 class ParallelQuickSort
 {
 public:
+	int* unsorted;
+	int* sorted;
+	size_t size;
+
 	ParallelQuickSort(int* unsorted_array);
 	int* sort();
-	void display();
+	void display(); // this is where all the fancy fancy stuff gon be shown
 
 private:
 	// this is where all the inner workings will be
+	void quicksort(int* arr, int low, int high);
+	int partition(int* arr, int low, int high); // returns index of pivot element
+	void swap(int* p1, int* p2);
 };
 
