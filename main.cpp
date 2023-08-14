68eb9bdb938d3240ab2d6cf3494d0c1b2a75ca4f
jim-low <robil12380@gmail.com>
2023-08-14 15:29:47 +0800

    chore: separate parallel quick sort code with normal quick sort code

--- main.cpp
+++ main.cpp
@@ -4,27 +4,49 @@
 #include "QuickSort.h"
 #include "ParallelQuickSort.h"
 
-int main()
+void performQuickSort(int* arr, size_t size)
 {
-	size_t size = 1000000;
-	int* arr = (int*)calloc(size, sizeof(int));
-	int size = sizeof(arr) / sizeof(int);
-
-	for (int i = 0; i < size; ++i)
-	{
-		arr[i] = -(int)size + (rand() % size);
-	}
-
 	QuickSort quicksort = QuickSort(arr, size);
 
 	clock_t start = clock();
 	quicksort.sort();
 	clock_t end = clock();
 
-	//quickSort.display();
+	//quicksort.display();
 	double elapsedTime = double(end - start) / CLOCKS_PER_SEC;
 
 	std::cout << "Time taken to sort " << size << " elements: " << elapsedTime << " seconds" << std::endl;
+}
+
+void performParallelQuickSort(int* arr, size_t size)
+{
+	QuickSort quicksort = QuickSort(arr, size);
+
+	clock_t start = clock();
+	quicksort.sort();
+	clock_t end = clock();
+
+	//quicksort.display();
+	double elapsedTime = double(end - start) / CLOCKS_PER_SEC;
+
+	std::cout << "Time taken to sort " << size << " elements with parallelism: " << elapsedTime << " seconds" << std::endl;
+}
+
+int main()
+{
+	size_t size = 100000000;
+	int* arr = (int*)calloc(size, sizeof(int));
+	//int size = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < size; ++i)
+	{
+		arr[i] = (rand() % size);
+	}
+
+	performParallelQuickSort(arr, size);
+	performQuickSort(arr, size);
+
+	//std::cout << "parallel quick sort has some problems (by some, i mean a lot)" << std::endl;
 
 	free(arr);
 
