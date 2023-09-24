391a56471153e16d4fdd87ff4d6816854e56fe13
yeesoon12 <86587298+yeesoon12@users.noreply.github.com>
2023-09-24 12:47:19 +0800

    removed 2 part

--- OMPParallelQuickSort.h
+++ OMPParallelQuickSort.h
@@ -11,8 +11,6 @@ public:
 	void sort();
 	void display();
 
-	void sort2();
-
 private:
 	size_t size;
 	float* unsorted; // this has the sole purpose of displaying the result of the sort
@@ -20,9 +18,5 @@ private:
 	void quicksort(float* arr, int low, int high);
 	int partition(float* arr, int low, int high);
 	void swap(float* p1, float* p2);
-
-	void quicksort2(float* arr, int low, int high);
-	int partition2(float* arr, int low, int high);
-	void swap2(float* p1, float* p2);
 };
 
