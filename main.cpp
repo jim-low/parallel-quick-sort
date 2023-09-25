0b536f69c9a8e89c102b3882fc9fd2f99c8c29f1
jim-low <robil12380@gmail.com>
2023-09-25 15:04:08 +0800

    feat: done

--- main.cpp
+++ main.cpp
@@ -24,96 +24,9 @@ float* generate_float_array(size_t size)
 	return arr;
 }
 
+
 int main(int argc, char** argv)
 {
-	srand(time(0));
-	size_t size = 10000; // max = 32768
-	float* arr = generate_float_array(size);
-
-	// MPI Parallel Quick Sort
-	//MPI_Init(&argc, &argv);
-	//int rank, numProcesses;
-	//MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-
-	//MPIParallelQuickSort mpiSort = MPIParallelQuickSort(arr, size, rank, numProcesses);
-	//auto start_time = std::chrono::high_resolution_clock::now();
-	//mpiSort.sort();
-	//auto end_time = std::chrono::high_resolution_clock::now();
-	//MPI_Barrier(MPI_COMM_WORLD);
-	//if (rank == 0)
-	//{
-	//	//mpiSort.display();
-	//	std::chrono::duration<double, std::milli> elapsed_time = end_time - start_time;
-	//	std::cout << "Elapsed time (MPI): " << elapsed_time.count() << " milliseconds" << std::endl;
-	//}
-	//MPI_Barrier(MPI_COMM_WORLD);
-
-	//OMPParallelQuickSort ompSort = OMPParallelQuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort.sort();
-	//ompSort.display();
-
-	//runtime = omp_get_wtime() - runtime;
-	// std::cout << "\n\nUsed " << runtime << " seconds." << std::endl;
-	//printf("\nUsed %.9f seconds.\n\n", runtime);
-
-	// Part for used to compare
-	//QuickSort ompSort2 = QuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort2.sort();
-	//ompSort2.display();
-
-	//runtime = omp_get_wtime() - runtime;
-
-	//printf("\nUsed %.9f seconds without OpenMP.\n\n", runtime);
-
-
-	//CUDA
-	size_t free_memory, total_memory;
-	cudaError_t cudaStatus = cudaMemGetInfo(&free_memory, &total_memory);
-	if (cudaStatus != cudaSuccess) {
-		std::cerr << "cudaMemGetInfo failed! Error: " << cudaGetErrorString(cudaStatus) << std::endl;
-		// Handle the error
-	}
-	else {
-		std::cout << "CUDA: Total GPU Memory: " << total_memory / (1024 * 1024) << " MB" << std::endl;
-		std::cout << "CUDA: Free GPU Memory: " << free_memory / (1024 * 1024) << " MB" << std::endl;
-	}
-
-	size_t data_size_bytes = size * sizeof(float);
-	double data_size_mb = static_cast<double>(data_size_bytes) / (1024 * 1024);
-	std::cout << "CUDA: Requirement to sort array: " << data_size_mb << " MB" << std::endl;
-
-
-	//CUDA Parallel Quick Sort
-	if (arr != nullptr) {
-
-		QuickSort standardSorter(arr, size);
-
-		CUDAParallelQuickSort CUDAsorter(arr, size);
-
-		cudaEvent_t start, stop;
-		cudaEventCreate(&start);
-		cudaEventCreate(&stop);
-
-		cudaEventRecord(start);
-		CUDAsorter.sort();
-		cudaEventRecord(stop);
-		cudaEventSynchronize(stop);
-
-		float milliseconds = 0;
-		cudaEventElapsedTime(&milliseconds, start, stop);
-		printf("CUDA took: %.2f ms\n", milliseconds);
-
-		CUDAsorter.display();
-
-	}
-	else {
-		std::cerr << "Error: Failed to allocate memory for arr." << std::endl;
-	}
-
-	MPI_Finalize();
-	free(arr);
+	printf("This is not used\n");
 	return 0;
 }
--- main.cu
+++ main.cu
@@ -3,13 +3,13 @@
 #include <vector>
 #include <mpi.h>
 #include <omp.h>
+#include <chrono>
 #include "QuickSort.h"
 #include "OMPParallelQuickSort.h"
 #include "MPIParallelQuickSort.h"
 #include "CUDAParallelQuickSort.cuh"
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include <chrono>
 
 float* generate_float_array(size_t size)
 {
@@ -36,90 +36,71 @@ float* generate_descending_array(size_t size)
 	return arr;
 }
 
-int main(int argc, char** argv)
+void standardQuicksortTest(float* arr, int size, bool display = false)
 {
-	srand(time(0));
-	size_t size = 10000000;
-	float* arr = generate_float_array(size);
+	QuickSort quicksort = QuickSort(arr, size);
+	auto start_time = std::chrono::high_resolution_clock::now();
+	quicksort.sort();
+	auto end_time = std::chrono::high_resolution_clock::now();
 
-	//for (int i = 0; i < size; ++i)
-	//{
-	//	std::cout << arr[i] << " ";
-	//}
-	//std::cout << std::endl;
-	//std::cout << std::endl;
-	//std::cout << std::endl;
+	if (display)
+	{
+		quicksort.display();
+	}
 
-	// MPI Parallel Quick Sort
-	//MPI_Init(&argc, &argv);
-	//MPIParallelQuickSort mpiSort = MPIParallelQuickSort(arr, size);
-	//mpiSort.sort();
-	//MPI_Finalize();
+	std::chrono::duration<double, std::milli> elapsed_time = end_time - start_time;
+	printf("Elapsed Time (Standard Sort): %.2f ms\n", elapsed_time);
+}
 
-	// OpenMP Part (need to comment the upper part to run)
-	//double runtime;
-	//omp_set_num_threads(8);
+void OMPSortTest(float* arr, int size, bool display = false)
+{
+	OMPParallelQuickSort ompSort = OMPParallelQuickSort(arr, size);
+	float start_time = omp_get_wtime();
+	ompSort.sort();
+	float end_time = omp_get_wtime();
 
-	//OMPParallelQuickSort ompSort = OMPParallelQuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort.sort();
-	////ompSort.display();
+	if (display)
+	{
+		ompSort.display();
+	}
+	float elapsed_time = end_time - start_time;
+	printf("Elapsed Time (OpenMP): %.2f ms\n", elapsed_time);
 
-	//runtime = omp_get_wtime() - runtime;
-	//// std::cout << "\n\nUsed " << runtime << " seconds." << std::endl;
-	//printf("\nUsed %.9f seconds.\n\n", runtime);
+}
 
-	//// Part for used to compare
-	//OMPParallelQuickSort ompSort2 = OMPParallelQuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort2.sort2();
-	////ompSort2.display();
+void MPISortTest(int* argc, char*** argv, float* arr, int size, int* rank, int* numProcesses, bool display = false)
+{
+	MPI_Init(argc, argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, rank);
+	MPI_Comm_size(MPI_COMM_WORLD, numProcesses);
 
-	//runtime = omp_get_wtime() - runtime;
+	if (*numProcesses <= 1)
+	{
+		printf("ERROR: This program needs at least 2 processors to execute.");
+		MPI_Finalize();
+		return;
+	}
 
-	//printf("\nUsed %.9f seconds without OpenMP.\n\n", runtime);
+	MPIParallelQuickSort mpiSort = MPIParallelQuickSort(arr, size, *rank, *numProcesses);
+	auto start_time = std::chrono::high_resolution_clock::now();
+	mpiSort.sort();
+	auto end_time = std::chrono::high_resolution_clock::now();
 
-	// OpenMP Part (need to comment the upper part to run)
-	//double runtime;
-	//omp_set_num_threads(8);
+	MPI_Finalize();
 
-	//OMPParallelQuickSort ompSort = OMPParallelQuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort.sort();
-	////ompSort.display();
+	if (*rank == 0)
+	{
+		if (display)
+		{
+			mpiSort.display();
+		}
+		std::chrono::duration<double, std::milli> elapsed_time = end_time - start_time;
+		printf("Elapsed Time (MPI Sort): %.2f ms\n", elapsed_time);
+	}
+}
 
-	//runtime = omp_get_wtime() - runtime;
-	//// std::cout << "\n\nUsed " << runtime << " seconds." << std::endl;
-	//printf("\nUsed %.9f seconds.\n\n", runtime);
-
-	//// Part for used to compare
-	//QuickSort ompSort2 = QuickSort(arr, size);
-	//runtime = omp_get_wtime();
-	//ompSort2.sort();
-	////ompSort2.display();
-
-	//runtime = omp_get_wtime() - runtime;
-
-	//printf("\nUsed %.9f seconds without OpenMP.\n\n", runtime);
-
-	//CUDA
-	//size_t free_memory, total_memory;
-	//cudaError_t cudaStatus = cudaMemGetInfo(&free_memory, &total_memory);
-	//if (cudaStatus != cudaSuccess) {
-	//	std::cerr << "cudaMemGetInfo failed! Error: " << cudaGetErrorString(cudaStatus) << std::endl;
-	//	// Handle the error
-	//}
-	//else {
-	//	std::cout << "CUDA: Total GPU Memory: " << total_memory / (1024 * 1024) << " MB" << std::endl;
-	//	std::cout << "CUDA: Free GPU Memory: " << free_memory / (1024 * 1024) << " MB" << std::endl;
-	//}
-
-	//size_t data_size_bytes = size * sizeof(float);
-	//double data_size_mb = static_cast<double>(data_size_bytes) / (1024 * 1024);
-	//std::cout << "CUDA: Requirement to sort array: " << data_size_mb << " MB" << std::endl;
-
-
-	//CUDA Parallel Quick Sort
+void CUDAQuicksortTest(float* arr, int size, bool display = true)
+{
 	if (arr != nullptr) {
 
 		QuickSort standardSorter(arr, size);
@@ -135,25 +116,40 @@ int main(int argc, char** argv)
 		cudaEventRecord(stop);
 		cudaEventSynchronize(stop);
 
+		if (display)
+		{
+			CUDAsorter.display();
+		}
+
 		float milliseconds = 0;
 		cudaEventElapsedTime(&milliseconds, start, stop);
 		printf("CUDA Sort Duration: %.2f ms\n", milliseconds);
-
-		//CUDAsorter.display();
-
-		auto start_time = std::chrono::high_resolution_clock::now();
-		standardSorter.sort();
-		auto end_time = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double, std::milli> elapsed_time = end_time - start_time;
-		std::cout << "\n\n Standard Sort Duration: " << elapsed_time.count() << "ms" << std::endl;
-		//standardSorter.display();
 		
 	}
-
 	else {
 		std::cerr << "Error: Failed to allocate memory for arr." << std::endl;
 	}
+}
 
+int main(int argc, char** argv)
+{
+	srand(time(0));
+	size_t size = 10;
+	float* arr = generate_float_array(size);
+
+	int rank = 0;
+	int numProcesses = 0;
+
+	//MPISortTest(&argc, &argv, arr, size, &rank, &numProcesses);
+
+	if (rank == 0)
+	{
+		standardQuicksortTest(arr, size, true);
+		std::cout << std::endl;
+		OMPSortTest(arr, size, true);
+		std::cout << std::endl;
+		//CUDAQuicksortTest(arr, size);
+	}
 
 	free(arr);
 	return 0;
